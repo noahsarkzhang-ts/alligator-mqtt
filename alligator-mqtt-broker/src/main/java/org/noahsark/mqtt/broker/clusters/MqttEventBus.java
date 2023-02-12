0e16b9c37ca42d3b2eb21d0ebca27449945ed751
noahsark <chuanzhang_ts@163.com>
2023-02-12 18:20:14 +0800

    添加方法注释

--- alligator-mqtt-broker/src/main/java/org/noahsark/mqtt/broker/clusters/MqttEventBus.java
+++ alligator-mqtt-broker/src/main/java/org/noahsark/mqtt/broker/clusters/MqttEventBus.java
@@ -14,11 +14,30 @@ import java.util.concurrent.TimeUnit;
  **/
 public interface MqttEventBus extends Lifecycle {
 
+    /**
+     * 向集群广播消息
+     * @param msg 消息
+     */
     void broadcast(ClusterMessage msg);
 
+    /**
+     * 接收集群消息
+     * @param msg 消息
+     */
     void receive(ClusterMessage msg);
 
+    /**
+     * 以阻塞方式获取消息
+     * @param timeout 超时时间
+     * @param unit 时间单位
+     * @return 消息
+     * @throws InterruptedException 异常
+     */
     ClusterMessage poll(long timeout, TimeUnit unit) throws InterruptedException;
 
+    /**
+     * 向本节点客户端推送消息
+     * @param message 消息
+     */
     void publish2Subscribers(PublishInnerMessage message);
 }
